1bd8b69cee5e0233f9c0e5260582f483612e8a0f
SAURYA78 <59814130+SAURYA78@users.noreply.github.com>
2020-12-27 17:59:20 +0530

    Create copy_module.py

---
+++ modules/copy_module.py
@@ -0,0 +1,15 @@
+sfile= input("Enter your source file:")
+dfile= input("Enter your destination file:")
+
+
+sfile="/home/ubuntu/Dockerfile"
+dfile="/home/ubuntu/dockerfile.txt"
+
+
+sfo= open(sfile,'r')
+content= sfo.read()
+sfo.close()
+
+dfo=open(dfile, 'w')
+dfo.write(content)
+dfo.close()
