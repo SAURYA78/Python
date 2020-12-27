5312cca859c87e6c483cc5d4b9946101288b0150
SAURYA78 <59814130+SAURYA78@users.noreply.github.com>
2020-12-27 17:55:40 +0530

    Create sysmodule.py

---
+++ modules/sysmodule.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+import random
+print(random.randrange(0,50,10))
+print(random.randint(0,50))
+
+
+root@ip-123-0-1-201:/home/ubuntu/python/modules# cat sysmodule.py
+#!/usr/bin/python3
+
+
+import sys
+
+print(sys.version)
+
+print(sys.argv)
