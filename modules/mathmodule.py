5b27898ef149b7ded4ce14e766d565d376c79789
SAURYA78 <59814130+SAURYA78@users.noreply.github.com>
2020-12-27 17:56:27 +0530

    Create mathmodule.py

---
+++ modules/mathmodule.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.asin(0.1))
+print(math.atan(0.7))
+print(math.acos(0.5))
+
+
+print(math.exp(2))
+print(math.log(2,10))
+
