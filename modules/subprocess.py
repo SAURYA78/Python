ba3fe4924e350b5fa59c762e2c1eb4cc1ab3953a
SAURYA78 <59814130+SAURYA78@users.noreply.github.com>
2020-12-27 18:00:32 +0530

    Update subprocess.py

--- modules/subprocess.py
+++ modules/subprocess.py
@@ -6,3 +6,21 @@ subprocess.run("free -m" shell=True)
 
 
 subprocess.call(['apt', 'install', 'nginx', '-y'])
+
+
+
+
+#!/usr/bin/python
+import os
+import subprocess
+#subprocess.run('free -m' shell=True)
+
+os.system('ls')
+print(os.popen('ls').read())
+
+subprocess.call(['ls','-al'])
+
+
+#input = 'command.py'
+#command= "cat {}".format(input)
+#subprocess.call('command')
